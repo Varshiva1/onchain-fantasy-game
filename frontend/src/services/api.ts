afb7f7bd04bdf40c1a224b21e20ef17146bb5894
Shubham <shubhamojha.ojha098@gmail.com>
2025-10-08 11:30:31 +0530

    fix: api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = (import.meta as any).env.VITE_BACKEND_URL || 'http://localhost:8080'
+const BASE_URL = (import.meta as any).env.VITE_BACKEND_URL || 'http://localhost:8000'
 
 export type Contest = {
   id: string
